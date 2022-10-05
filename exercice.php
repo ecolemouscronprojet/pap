ae193c4e9fd459ebcb81a7b3da6df175dbaff3e0
Samuel Lassoie <samuel.lassoie@gmail.com>
2022-10-05 19:03:31 +0200

    wip

--- exercice.php
+++ exercice.php
@@ -3,10 +3,23 @@
 $nb1 = 10;
 $nb2 = 15;
 
-// COUCOU
-
+$temp = $nb1;
+$nb1 = $nb2;
+$nb2 = $temp;
 
 // RESULTAT
 echo $nb1;
 echo '<br>';
 echo $nb2;
+
+
+$prenom = 'Bob';
+
+
+echo '<br>';
+echo "Hello comment ça va $prenom ?<br>";
+echo 'Hello comment ça va ' . $prenom . ' ?<br>';
+echo 'Hello comment ça va ' , $prenom . ' ?<br>';
+
+
+

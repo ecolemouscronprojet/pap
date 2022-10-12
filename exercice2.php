9768ba55250e7101b5fc0a446763d5272b03f9d1
Samuel Lassoie <samuel.lassoie@gmail.com>
2022-10-12 21:07:42 +0200

    wip

---
+++ exercice2.php
@@ -0,0 +1,12 @@
+<?php
+$nb = 7;
+echo "1 x $nb = " . (1 * $nb) . "<br>";
+echo "2 x $nb = " . (2 * $nb) . "<br>";
+echo "3 x $nb = " . (3 * $nb) . "<br>";
+echo "4 x $nb = " . (4 * $nb) . "<br>";
+echo "5 x $nb = " . (5 * $nb) . "<br>";
+echo "6 x $nb = " . (6 * $nb) . "<br>";
+echo "7 x $nb = " . (7 * $nb) . "<br>";
+echo "8 x $nb = " . (8 * $nb) . "<br>";
+echo "9 x $nb = " . (9 * $nb) . "<br>";
+echo "10 x $nb = " . (10 * $nb) . "<br>";

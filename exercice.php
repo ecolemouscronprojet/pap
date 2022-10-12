2fa0e5c92591062d6793536a2aef0528343ab256
Samuel Lassoie <samuel.lassoie@gmail.com>
2022-10-12 18:59:45 +0200

    wip

--- exercice.php
+++ exercice.php
@@ -29,9 +29,15 @@ echo '<h1>Exercice 2</h1>';
 $mot1 = 'acheté';
 $mot2 = 'maison';
 $mot3 = 'une';
-$mot4= 'j\'ai';
-$mot5= 'Mouscron';
-$mot6= 'à';
+$mot4 = 'j\'ai';
+$mot5 = 'Mouscron';
+$mot6 = 'à';
 
+
+echo $mot4 . ' ' . $mot1 . ' ' . $mot3 . ' ' . $mot2 . ' ' . $mot6 . ' ' . $mot5;
+
+
+echo 'ma voiture est rouge, ma voiture est noire, ma voiture est verte,
+ma voiture est orange';
 
 

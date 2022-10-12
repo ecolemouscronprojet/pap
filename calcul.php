cfcb9b096c9f5a3bb3e65302b00a300ff4b92cb9
Samuel Lassoie <samuel.lassoie@gmail.com>
2022-10-12 20:32:15 +0200

    wip

---
+++ calcul.php
@@ -0,0 +1,61 @@
+<?php
+
+echo '<h1>calcul</h1>';
+
+echo '<h2>Addition</h2>';
+
+$nb1 = 5;
+$nb2 = 147;
+
+echo $nb1 + $nb2;
+echo '<br>';
+
+$nb1 = $nb1 + $nb2;
+
+echo $nb1;
+
+echo '<h2>Multiplication</h2>';
+
+$nb1 = 30;
+$nb2 = 20;
+$nb3 = 6;
+
+echo $nb1 * $nb2;
+
+// 606
+echo '<br>';
+echo $nb1 * $nb2 + $nb3;
+echo '<br>';
+// 150
+echo $nb2 * $nb3 + $nb1;
+
+echo '<h2>Division</h2>';
+
+$nb1 = 8;
+$nb2 = 160;
+
+echo $nb2 / $nb1;
+
+
+echo '<h2>Soustraction</h2>';
+
+$nb1 = 8;
+$nb2 = 160;
+
+echo $nb2 - $nb1;
+
+
+echo '<h2>Modulo</h2>';
+
+$nb1 = 60;
+$nb2 = 8;
+
+echo $nb1 % $nb2;
+
+
+
+
+
+
+
+

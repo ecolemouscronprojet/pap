6f54603c00fb555d98cd1972232f599defe27fb0
Samuel Lassoie <samuel.lassoie@gmail.com>
2022-10-12 20:38:01 +0200

    wip

--- calcul.php
+++ calcul.php
@@ -10,7 +10,8 @@ $nb2 = 147;
 echo $nb1 + $nb2;
 echo '<br>';
 
-$nb1 = $nb1 + $nb2;
+// $nb1 = $nb1 + $nb2;
+$nb1 += $nb2;
 
 echo $nb1;
 
@@ -50,7 +51,15 @@ echo '<h2>Modulo</h2>';
 $nb1 = 60;
 $nb2 = 8;
 
-echo $nb1 % $nb2;
+echo $nb1 % $nb2 .'<br>';
+
+echo 1 % 3 .' | 1<br>';
+echo 2 % 3 .' | 2<br>';
+echo 3 % 3 .' | 3<br>';
+echo 4 % 3 .' | 4<br>';
+echo 5 % 3 .' | 5<br>';
+echo 6 % 3 .' | 6<br>';
+echo 7 % 3 .' | 7<br>';
 
 
 

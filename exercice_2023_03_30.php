c6eb7b637118b99ce3b3662102d3f99c7648bd27
Samuel Lassoie <samuel.lassoie@gmail.com>
2023-03-30 19:06:02 +0200

    wip

--- exercice_2023_03_30.php
+++ exercice_2023_03_30.php
@@ -18,9 +18,29 @@ echo '<h1>Exercice n°1</h1>';
 
 // -----------AVEC FONCTION--------
 
-function convert_celsius_to_fahrenheit($celsius) {
-    $response = $celsius * (9/5) + 32;
+function convert_celsius_to_fahrenheit($celsius)
+{
+    $response = $celsius * (9 / 5) + 32;
     return $response;
 }
 
+
 echo convert_celsius_to_fahrenheit(10);
+
+
+echo '<h1>Exercice n°2</h1>';
+// créez une fonction qui permet de retourner la puissance de 2 d'un nombre
+
+function puissanceDe2($nb)
+{
+    return $nb * $nb;
+}
+
+
+echo puissanceDe2(5);
+
+echo '<h1>Exercice n°3</h1>';
+// créez une fonction dans laquelle vous passez le tableau suivant et où la
+//fonction doit retourner le nombre le plus grand du tableau
+
+$nbs = [10, 22, 100, 33, 44, 2, 288, 6, 7];

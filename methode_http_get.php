8d3bc399fc45a71fed28b41b8166abaac43a7722
Samuel Lassoie <samuel.lassoie@gmail.com>
2023-04-13 19:34:04 +0200

    wip

---
+++ methode_http_get.php
@@ -0,0 +1,24 @@
+<?php
+//?fruit1=pomme&fruit2=poire
+
+// echo $_GET["fruit1"] ?? '';
+// echo '<br>';
+// echo $_GET["fruit2"] ?? '';
+
+// if (isset($_GET['fruit1'])) {
+//     $f1 = $_GET['fruit1'];
+//     echo $f1;
+// }
+
+// echo '<br>';
+// if (isset($_GET['fruit2'])) {
+//     $f2 = $_GET['fruit2'];
+//     echo $f2;
+// }
+
+echo '<h2>Exercice N°2</h2>'
+//?nb1=5&nb2=10
+
+//nb1
+
+//nb2

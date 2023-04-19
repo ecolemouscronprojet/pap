2741dbd9ae1abaa47d49e1eb0934bac5559b4eb2
Samuel Lassoie <samuel.lassoie@gmail.com>
2023-04-19 19:02:32 +0200

    wip

--- calculatrice.php
+++ calculatrice.php
@@ -8,6 +8,15 @@ if(isset($_GET["num1"], $_GET["num2"], $_GET["operateur"])){
     $num2 = (int) $_GET["num2"];
     $operateur = $_GET["operateur"];
     if(in_array($operateur, $allOperateurs)) {
+        if($operateur === 'plus'){
+            echo $num1 + $num2 .'<br>';
+        } elseif($operateur === 'moins'){
+            echo $num1 - $num2 .'<br>';
+        } elseif($operateur === 'divise'){
+            echo $num1 / $num2 .'<br>';
+        } elseif($operateur === 'multiplier'){
+            echo $num1 * $num2 .'<br>';
+        }
 
     } else {
         echo "l'opérateur que vous avez saisi n'est pas valide !";
